0a266bab650e5f9785a5c75fc8bec61867da39d7
imwaykul <imwaykul@gmail.com>
2015-09-25 23:35:48 -0400

    Create PopulationTrend.java

---
+++ PopulationTrend.java
@@ -0,0 +1,33 @@
+public class PopulationTrend {
+    private String size;
+    private long population;
+    public PopulationTrend(String countrySize) {
+        size = countrySize;
+    }
+    public long popSize() {
+        if (size == "small") {
+            population = 3500000L;
+        }
+        else if (size == "medium") {
+            population = 35000000L;
+        }
+        else if (size == "large") {
+            population = 350000000L;
+        }
+        else if (size == "colossal") {
+            population = 3500000000L;
+        }
+        return population;
+    }
+    public static void main(String [] args) {
+        PopulationTrend popOne = new PopulationTrend("colossal");
+        long sizing = popOne.popSize();
+        System.out.println("POPULATION SIZE: " + sizing);
+    }
+}
+
+
+
+// generate a random population
+// small, medium, large, colossal country
+// population based on that
