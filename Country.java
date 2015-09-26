3ca451d547d0a3fef86d0c67f4f688e99453c5bf
rzd83091 <richard.z.deng@gmail.com>
2015-09-26 01:18:09 -0400

    Edited Countrymaking.java

--- Country.java
+++ Country.java
@@ -3,12 +3,26 @@ public class Country {
 	private String cName;
 	private String fName;
 	private Float pop;
+	private double budget;
 	
-	public Country(String countryName,String faceName,Float population){
+	public Country(){}
+	
+	public Country(String countryName,String faceName,Float population,double budget){
 	this.cName = countryName;
 	this.fName = faceName;
 	this.pop = population;
+	this.budget = budget;
 	
+	
+	}
+	
+	public void Update(){
+		
+	}
+	
+	public void print(){
+		String out = "Name: " +cName + "\nLeader: " + fName + "\nPopulation: " + pop + "\nBudget: " + budget;
+		System.out.println(out);
 	}
 
 }
--- Countrymaking.java
+++ Countrymaking.java
@@ -1,9 +1,20 @@
-
+import java.util.Scanner;
 public class Countrymaking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Country userCountry = new Country();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Name: ");
+		String name = in.next();
+		System.out.println("Leader Name: ");
+		String lName = in.next();
+		System.out.println("Population: ");
+		Float pop = in.nextFloat();
+		System.out.println("Budget: ");
+		double bud = in.nextDouble();
 		
+		userCountry = new Country(name,lName,pop,bud);
+		userCountry.print();
 	}
 
 }
