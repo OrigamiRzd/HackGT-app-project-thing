c76fcf2f2b3c0355c6a17f73915e4b6235591b3e
rzd83091 <richard.z.deng@gmail.com>
2015-09-26 15:36:13 -0400

    Changed rates from double to float

--- Country.java
+++ Country.java
@@ -4,17 +4,19 @@ public class Country {
 	private String cName;
 	private String fName;
 	private Float pop;
+	private float popGrowthRate;
 	private float budget;
-	private float taxRate;
+	private double taxRate;
 	private float gdp;
 	private float gdpGrowthRate;
 	private static int year;
+	
 	
 	public Country(){}
 	
 	Random randOj = new Random();
 	
-	public Country(String countryName,String faceName,String pSize,float gdp, float gdpGRate){
+	public Country(String countryName,String faceName,String pSize,float gdp/*, double gdpGRate*/){
 	PopulationTrend pT = new PopulationTrend(pSize);
 	this.cName = countryName;
 	this.fName = faceName;
@@ -22,7 +24,8 @@ public class Country {
 	this.budget = 0;
 	this.taxRate = .01f;//[0 to .05]
 	this.gdp = gdp;
-	this.gdpGrowthRate = gdpGRate;//[-.2 to .2] make a bell curve random pick thing
+	
+	//this.gdpGrowthRate = gdpGRate;//[-.2 to .2] make a bell curve random pick thing
 	year=0;
 	}
 	
@@ -50,12 +53,16 @@ public class Country {
 		return this.gdpGrowthRate;
 	}
 	
-	public void setPop(Float population){
+	public void setPop(float population){
 		this.pop = population;
 	}
 	
 	public Float getPop(){
 		return pop;
+	}
+	
+	public void setPopRate(float pr){
+		this.popGrowthRate = pr;
 	}
 	
 	public int getYear(){
@@ -66,16 +73,15 @@ public class Country {
 		
 		float growthRate = randOj.nextFloat() * 20 - 10;
 		System.out.println();
-		budget = budget + this.gdp*taxRate;//Budget calc
+		budget = budget + (float)(this.gdp*taxRate);//Budget calc
 		
 		this.gdp = this.gdp - budget;
 		this.gdp = this.gdp + this.gdp*(growthRate/100);
         
-        float popGrowth = randOj.nextFloat() * 10 - 5;
-        float popRate = popGrowth/100;
-        pop = pop + pop*popRate;
+        float popGrowthRate = (randOj.nextFloat() * 10 - 5)/100;
+        pop = pop + pop*popGrowthRate;
 	
-        System.out.println(growthRate + "\n" +popGrowth);
+        System.out.println(growthRate + "\n" +popGrowthRate);
 		
 	year++;
 	}
